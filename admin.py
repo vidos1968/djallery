d6d862a476e2b237e7b163c7fb2b1ee3bace1dcf
Stepan <xevinbox@yandex.ru>
2015-06-30 23:32:19 +0500

    add inline photo adding in admin

--- admin.py
+++ admin.py
@@ -3,14 +3,25 @@
 from django.contrib import admin
 from models import Photo, Album
 
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+    extra = 0
+    readonly_fields = ('image_thumb',)
+    fields = [ 'image_thumb', 'img', 'alt', 'album_cover', ]
+
+
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('image_thumb', 'description', 'alt', 'group', )
 
     list_filter = ('group',)
     ordering = ['group']
 
+
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    inlines = (PhotoInline, )
+
 
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Album, AlbumAdmin)
